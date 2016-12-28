3e32f53bd56b6933a62c378db14b93540f28a3f8
huhan <huhan@mixlinker.com>
2016-12-28 19:01:07 +0800

    ```

---
+++ app/src/main/java/com/example/administrator/newsdemo/presenter/contract/MainContract.java
@@ -0,0 +1,19 @@
+package com.example.administrator.newsdemo.presenter.contract;
+
+import com.example.administrator.newsdemo.base.BasePresenter;
+import com.example.administrator.newsdemo.base.BaseView;
+
+/**
+ * Created by Administrator on 2016/12/28.
+ */
+
+public interface MainContract {
+
+    interface  View extends BaseView{
+
+    }
+
+    interface Presenter extends BasePresenter<View>{
+
+    }
+}
--- app/src/main/java/com/example/administrator/newsdemo/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/newsdemo/ui/activity/MainActivity.java
@@ -7,10 +7,11 @@ import com.example.administrator.newsdemo.R;
 import com.example.administrator.newsdemo.base.BaseActivity;
 import com.example.administrator.newsdemo.base.BasePresenter;
 import com.example.administrator.newsdemo.presenter.MainPresenter;
+import com.example.administrator.newsdemo.presenter.contract.MainContract;
 
 import butterknife.BindView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.View{
     @BindView(R.id.textview)
     TextView textView;
 
@@ -20,9 +21,10 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
-    protected BasePresenter getPresenter() {
+    protected MainPresenter getPresenter() {
         return new MainPresenter();
     }
+
 
     @Override
     public void showError(String msg) {
